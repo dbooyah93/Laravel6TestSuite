572a3acb3703feee2245836a738a704336ea9b6f
David Boyea <dboyea@cimlbr.com>
2021-01-21 14:32:43 -0600

    Commit Tests with artisan version control

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/posts/{post}', 'PostsController@show');
+
+Route::get('/{endPoint}', function ($endPoint) {
+    if ($endPoint === 'test') {
+        $name = 'David Boyea';
+    }
+    return view($endPoint, [
+        'user' => $name
+    ]);
+});
+
+
+// Route::get('/', function () {
+//     $name = request('name');
+    
+//     return view('welcome', [
+//         'name' => $name
+//     ]);
+// });
+
+// Route::get('/test', function () {
+//     return view('test');
+// });
+
+// Route::get('/testing', function () {
+//     return [ 'array' => 'here' ];
+// });
